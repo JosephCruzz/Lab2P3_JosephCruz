80150b1be20e741663e181eba1023e51fa22552a
JosephCruzz <josephcruz0503@gmail.com>
2023-01-27 14:59:15 -0600

    Commit #3

--- Lab2P3_JosephCruz/Lab2P3_JosephCruz.cpp
+++ Lab2P3_JosephCruz/Lab2P3_JosephCruz.cpp
@@ -11,11 +11,12 @@ int main()
 		cout << "3. Ejercicio practico 3 - Diferencia entre fechas" << endl;
 		cout << "4. Salir." << endl;
 		cout << "Ingrese la opcion que desea: ";  cin >> menu;
-		switch (menu) 
-			{
+		switch (menu)
+		{
 		case 1:
 		{
 			int aguinaldo = 0;
+			int aguinaldoBono = 0;
 			int sueldoEnero = 0;
 			int sueldoFebrero = 0;
 			int sueldoMarzo = 0;
@@ -40,30 +41,54 @@ int main()
 			int diasOctubre = 0;
 			int diasNoviembre = 0;
 			int diasDiciembre = 0;
+			int diasFaltadosProm = 0;
+			cout << "*******************************************" << endl;
 			cout << "Bienvenido al calculo de aguinaldo" << endl;
-			cout << "Ingrese su sueldo bruto del mes de Enero "; cin >> sueldoEnero;
-			cout << "Ingrese total de dias faltados del mes de Enero "; cin >> diasEnero;
-			cout << "Ingrese su sueldo bruto del mes de Febrero "; cin >> sueldoFebrero;
-			cout << "Ingrese total de dias faltados del mes de Febrero "; cin >> diasFebrero;
-			cout << "Ingrese su sueldo bruto del mes de Marzo "; cin >> sueldoMarzo;
-			cout << "Ingrese total de dias faltados del mes de Marzo "; cin >> diasMarzo;
-			cout << "Ingrese su sueldo bruto del mes de Abril "; cin >> sueldoAbril;
-			cout << "Ingrese total de dias faltados del mes de Abril "; cin >> diasAbril;
-			cout << "Ingrese su sueldo bruto del mes de Mayo "; cin >> sueldoMayo;
-			cout << "Ingrese total de dias faltados del mes de Mayo "; cin >> diasMayo;
-			cout << "Ingrese su sueldo bruto del mes de Junio "; cin >> sueldoJunio;
-			cout << "Ingrese su sueldo bruto del mes de Julio "; cin >> sueldoJulio;
-			cout << "Ingrese su sueldo bruto del mes de Agosto "; cin >> sueldoAgosto;
-			cout << "Ingrese su sueldo bruto del mes de Septiembre "; cin >> sueldoSeptiembre;
-			cout << "Ingrese su sueldo bruto del mes de Octubre "; cin >> sueldoOctubre;
-			cout << "Ingrese su sueldo bruto del mes de Noviembre "; cin >> sueldoNoviembre;
-			cout << "Ingrese su sueldo bruto del mes de Diciembre "; cin >> sueldoDiciembre;
-			cout << "Ingrese la cantidad de dias trabajados "; cin >> dias;
-			cout << "Ingrese total de dias faltados por mes: "; cin >> dias;
-			aguinaldo = sueldo * dias;
+			cout << "*******************************************" << endl;
+			cout << "Ingrese su sueldo bruto del mes de Enero: "; cin >> sueldoEnero;
+			cout << "Ingrese total de dias faltados del mes de Enero: "; cin >> diasEnero;
+			cout << "Ingrese su sueldo bruto del mes de Febrero: "; cin >> sueldoFebrero;
+			cout << "Ingrese total de dias faltados del mes de Febrero: "; cin >> diasFebrero;
+			cout << "Ingrese su sueldo bruto del mes de Marzo: "; cin >> sueldoMarzo;
+			cout << "Ingrese total de dias faltados del mes de Marzo: "; cin >> diasMarzo;
+			cout << "Ingrese su sueldo bruto del mes de Abril: "; cin >> sueldoAbril;
+			cout << "Ingrese total de dias faltados del mes de Abril: "; cin >> diasAbril;
+			cout << "Ingrese su sueldo bruto del mes de Mayo: "; cin >> sueldoMayo;
+			cout << "Ingrese total de dias faltados del mes de Mayo: "; cin >> diasMayo;
+			cout << "Ingrese su sueldo bruto del mes de Junio: "; cin >> sueldoJunio;
+			cout << "Ingrese total de dias faltados del mes de Junio: "; cin >> diasJunio;
+			cout << "Ingrese su sueldo bruto del mes de Julio: "; cin >> sueldoJulio;
+			cout << "Ingrese total de dias faltados del mes de Julio: "; cin >> diasJulio;
+			cout << "Ingrese su sueldo bruto del mes de Agosto: "; cin >> sueldoAgosto;
+			cout << "Ingrese total de dias faltados del mes de Agosto: "; cin >> diasAgosto;
+			cout << "Ingrese su sueldo bruto del mes de Septiembre: "; cin >> sueldoSeptiembre;
+			cout << "Ingrese total de dias faltados del mes de Septiembre: "; cin >> diasSeptiembre;
+			cout << "Ingrese su sueldo bruto del mes de Octubre: "; cin >> sueldoOctubre;
+			cout << "Ingrese total de dias faltados del mes de Octubre: "; cin >> diasOctubre;
+			cout << "Ingrese su sueldo bruto del mes de Noviembre: "; cin >> sueldoNoviembre;
+			cout << "Ingrese total de dias faltados del mes de Noviembre: "; cin >> diasNoviembre;
+			cout << "Ingrese su sueldo bruto del mes de Diciembre: "; cin >> sueldoDiciembre;
+			cout << "Ingrese total de dias faltados del mes de Diciembre: "; cin >> diasDiciembre;
+			aguinaldo = sueldoEnero + sueldoFebrero + sueldoMarzo + sueldoAbril + sueldoMayo + sueldoJunio + sueldoJulio + sueldoAgosto + sueldoSeptiembre + sueldoOctubre + sueldoNoviembre + sueldoDiciembre;
+			aguinaldo = aguinaldo / 12;
 			cout << "El aguinaldo es: " << aguinaldo << endl;
+			aguinaldoBono = sueldoJulio + sueldoAgosto + sueldoSeptiembre + sueldoOctubre + sueldoNoviembre + sueldoDiciembre;
+			aguinaldoBono = aguinaldoBono / 6;
+			diasFaltadosProm = (diasJulio + diasAgosto + diasSeptiembre + diasOctubre + diasNoviembre + diasDiciembre);
+			diasFaltadosProm = diasFaltadosProm / 6;
+			if (diasFaltadosProm <= 2) {
+				aguinaldoBono =  (aguinaldoBono * 0.05);
+				cout << "El bono es: " << aguinaldoBono << endl;
+			}
+			else {
+				cout << "No hay Bonificacion." << endl;
+			}
+			
 			break;
 		}
+		case 2:
+		{}
+			break;
 				
 		}
     } while (menu!= 4);
